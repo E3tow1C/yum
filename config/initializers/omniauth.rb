27fc3825e25cc6ed7af7ca4f57ba8917d9e32017
E3tow1C <thepphithak.w@kkumail.com>
2025-05-06 13:49:02 +0700

    it works!!

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,16 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :keycloak_openid, ENV["KEYCLOAK_CLIENT_ID"], ENV["KEYCLOAK_CLIENT_SECRET"],
-    client_options: { site: ENV["KEYCLOAK_SITE"], realm: ENV["KEYCLOAK_REALM"] },
-    name: "keycloak"
+  provider :keycloak_openid,
+    ENV["KEYCLOAK_CLIENT_ID"],
+    ENV["KEYCLOAK_CLIENT_SECRET"],
+    name: "keycloak_openid",
+    client_options: {
+      base_url: "",
+      site: ENV["KEYCLOAK_SITE"],
+      realm: ENV["KEYCLOAK_REALM"],
+      authorize_url: "/auth/realms/#{ENV["KEYCLOAK_REALM"]}/protocol/openid-connect/auth",
+      token_url: "/auth/realms/#{ENV["KEYCLOAK_REALM"]}/protocol/openid-connect/token",
+      userinfo_url: "/auth/realms/#{ENV["KEYCLOAK_REALM"]}/protocol/openid-connect/userinfo"
+    }
 end
 
 # Prevent OmniAuth from using /auth/failure for failed authentication
