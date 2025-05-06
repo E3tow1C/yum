8ceb71cf9ec8ac60bdea57d68586b4a281fe9f0d
E3tow1C <thepphithak.w@kkumail.com>
2025-05-06 21:47:05 +0700

    add scope "openid" to make keycloak send id_token back to rails

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,16 +2,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :keycloak_openid,
     ENV["KEYCLOAK_CLIENT_ID"],
     ENV["KEYCLOAK_CLIENT_SECRET"],
-    name: "keycloak_openid",
+    scope: "openid",
     client_options: {
       base_url: "",
       site: ENV["KEYCLOAK_SITE"],
-      realm: ENV["KEYCLOAK_REALM"],
-      authorize_url: "/auth/realms/#{ENV["KEYCLOAK_REALM"]}/protocol/openid-connect/auth",
-      token_url: "/auth/realms/#{ENV["KEYCLOAK_REALM"]}/protocol/openid-connect/token",
-      userinfo_url: "/auth/realms/#{ENV["KEYCLOAK_REALM"]}/protocol/openid-connect/userinfo",
-      end_session_endpoint: "/auth/realms/#{ENV["KEYCLOAK_REALM"]}/protocol/openid-connect/logout"
-    }
+      realm: ENV["KEYCLOAK_REALM"]
+    },
+    name: "keycloak_openid"
 end
 
 # Prevent OmniAuth from using /auth/failure for failed authentication
