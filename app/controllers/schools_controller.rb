627deeda27f4a5aea40d502f79c1da78685c9675
E3tow1C <thepphithak.w@kkumail.com>
2025-05-07 14:57:49 +0700

    Add user authentication before actions in Courses and Schools controllers

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,5 +1,6 @@
 class SchoolsController < ApplicationController
   before_action :set_school, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /schools or /schools.json
   def index
