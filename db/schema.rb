e3e0c9b93e22fd267b989e37879e7b5370b5ad55
E3tow1C <thepphithak.w@kkumail.com>
2025-05-07 14:32:54 +0700

    add courses MVC

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_06_041030) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_07_073236) do
+  create_table "courses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "provider"
     t.string "uid"
